f1e22036995e83afc9c7c673aaf83ed7d0befddb
Alexander kagerer <kagerer19@gmail.com>
2020-04-26 18:16:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,10 +8,10 @@ function basicTeenager (age) {
 
 
 function teenager(age) {
-  if ( age >=13 && age <=19){
+  if ( age >=13 || age <=19 && >=20 ){
  	 return ("You are a teenager!");
 	}else {
-  return ("You are not a teenager");
+  return ("You are a grownup");
 	}
 
 }
