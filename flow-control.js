7a12d4f77d287ef36e072fec5827dfe554eabb6d
Alexander kagerer <kagerer19@gmail.com>
2020-04-26 18:13:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,11 +21,7 @@ function ageChecker(age) {
  	 return ("You are a teenager!");
 	}else {
   return ("You are a kid");
-  else if (age >=20 ){
-    return ("you a grown up")
-  }
-  
-	}
+ }
 }
 
 function ternaryTeenager(age) {
