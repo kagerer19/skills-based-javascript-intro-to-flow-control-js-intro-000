1796e7cd0a67a021ec81d7da3c1c671f8d89f7dd
Alexander kagerer <kagerer19@gmail.com>
2020-04-26 19:30:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -40,7 +40,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   if ( age >=13 && age <=19){
- 	 return ("You are a teenager");
+ 	 return ("You have an age");
 	}else {
   return ("You are not a teenager");
 	}
